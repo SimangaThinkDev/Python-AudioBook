75998c3a845f9645677145c4ba7299167d132ced
Simangaliso Phakwe <siphakwjhb024@student.wethinkcode.co.za>
2025-04-18 15:40:11 +0200

    Adding main file, which controls the whole logic of the program

---
+++ main.py
@@ -0,0 +1,11 @@
+# MAke sure you have installed the library gTTS
+# [ pip install gTTS ]
+from gtts import gTTS
+import os
+
+
+
+def create_audiobook( text_file, output ):
+    
+    # Open the file you would like to turn into an audio book
+    return 0 
